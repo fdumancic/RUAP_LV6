8495c1ca581e09f1b3b289f42f91e5478b55d7e4
fdumancic <dumancic.filip@gmail.com>
2017-05-16 16:43:39 +0200

    Add files via upload
    zadatak 1

---
+++ lv6zad1/lv6zad1/Program.cs
@@ -0,0 +1,85 @@
+﻿// This code requires the Nuget package Microsoft.AspNet.WebApi.Client to be installed.
+// Instructions for doing this in Visual Studio:
+// Tools -> Nuget Package Manager -> Package Manager Console
+// Install-Package Microsoft.AspNet.WebApi.Client
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallRequestResponseService
+{
+
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvokeRequestResponseService().Wait();
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() { 
+                        { 
+                            "input1", 
+                            new StringTable() 
+                            {
+                                ColumnNames = new string[] {"MPG", "Cyl", "Displacement", "Horsepower", "Weight", "Acceleration", "Year", "CountryCode", "Model"},
+                                Values = new string[,] {  { "18", "8", "307", "130", "3504", "23", "70", "1", "chevrolet chevelle malibu" },  { "21", "6", "200", "85", "2587", "16", "70", "1", "ford maverick" },  }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+                const string apiKey = "2BkmDUc6kRNlFeg8Gqq5tUGRxeU8Tnkf+lbt21D9jO0rgB3es+OKQuHVGi4IIjsOOUn1VEqcYWEmE6zK0crRsw=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/a077ab2a72414aa6b9a8c121c76019ff/services/2280d864ac4044cdbe8891a7d4dbcc11/execute?api-version=2.0&details=true");
+
+                // WARNING: The 'await' statement below can result in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
